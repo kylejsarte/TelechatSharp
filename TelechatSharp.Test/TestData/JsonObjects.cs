6afca1b2cea5c31b51a14dfa8821a7729bd905a9
Kyle Sarte <kylejsarte@gmail.com>
2024-02-08 20:23:22 -0600

    Add test data

---
+++ TelechatSharp.Test/TestData/JsonObjects.cs
@@ -0,0 +1,29 @@
+﻿namespace TelechatSharp.Test.TestData
+{
+    public static class JsonObjects
+    {
+        public static IEnumerable<object[]> JsonArrays => new[]
+        {
+            new object[]
+            {
+                new object[] { new { type = "", text = "text entity" } },
+                "text entity"
+            },
+            new object[]
+            {
+                new object[] { "plain text ", new { type = "", text = "text entity" } },
+                "plain text text entity"
+            },
+            new object[]
+            {
+                new object[] { new { type = "", text = "text entity" }, " plain text" },
+                "text entity plain text"
+            },
+            new object[]
+            {
+                new object[] { "plain text ", new { type = "", text = "text entity" }, " plain text" },
+                "plain text text entity plain text"
+            },
+        };
+    }
+}
