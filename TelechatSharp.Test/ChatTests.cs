0850b9dd830605f3415d430afa66d96c6f9fbd72
Kyle Sarte <kylejsarte@gmail.com>
2024-02-08 20:21:15 -0600

    Add Chat unit tests

--- TelechatSharp.Test/ChatTests.cs
+++ TelechatSharp.Test/ChatTests.cs
@@ -1,6 +1,37 @@
+using TelechatSharp.Core;
+
 namespace TelechatSharp.Test
 {
     public class ChatTests
     {
+        private string TestChatJsonPath => Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "TestData", "test_chat.json");
+
+        [Fact]
+        public void ChatConstructor_InitializesChat_WithFilePath()
+        {
+            var chat = new Chat(TestChatJsonPath);
+
+            Assert.NotNull(chat);
+
+            Assert.Equal("TEST_NAME", chat.Name);
+            Assert.Equal("TEST_TYPE", chat.Type);
+            Assert.Equal(0, chat.Id);
+            Assert.NotNull(chat.Messages);
+        }
+
+        [Fact]
+        public void ChatConstructor_InitializesChat_WithStreamReader()
+        {
+            using var streamReader = File.OpenText(TestChatJsonPath);
+
+            var chat = new Chat(streamReader);
+
+            Assert.NotNull(chat);
+
+            Assert.Equal("TEST_NAME", chat.Name);
+            Assert.Equal("TEST_TYPE", chat.Type);
+            Assert.Equal(0, chat.Id);
+            Assert.NotNull(chat.Messages);
+        }
     }
 }
